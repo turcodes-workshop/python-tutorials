99ac0017d8812627ff54f1126f5997f0af9764d5
Hasan Türkmen <wd.devcodec@gmail.com>
2022-02-27 15:06:31 +0300

    string_formats
    string format examples.

---
+++ string_formats.py
@@ -0,0 +1,12 @@
+name = "Hasan"
+surname = "Türkmen"
+age = 26
+
+print("My name is {1} {0}".format(name, surname))
+
+print("My name is {n} {s}. I'm {a} years old".format(n=name, s=surname, a=age))
+
+number = 5 /3
+print("the result is {n:1.4}".format(n=number))
+
+print(f"{number:1.5}") # f keywordü ile text içersiine variable ekleyebiliyoruz.
