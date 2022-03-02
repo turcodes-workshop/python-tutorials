b76c59bf6003e0b9715c12e025ea46b1863b91d3
Hasan Türkmen <wd.devcodec@gmail.com>
2022-03-02 23:28:16 +0300

    assigments
    assigment examples

---
+++ assigments.py
@@ -0,0 +1,19 @@
+a = 10
+b = 20
+c = 30
+
+a, b, c = 10,20,30
+a,b = b,a
+print(a,b,c)
+
+a= a+5
+a += 5
+a -= 5
+a *= 5
+a %= 3
+a **=5
+a //=5
+
+numbers = (1,2,3,4,5,6)
+a, *b, c = numbers
+print(a,b,c)
