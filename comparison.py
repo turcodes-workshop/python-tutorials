ad1cec384291b8d8546a4be2a185580159ed6f68
Hasan Türkmen <wd.devcodec@gmail.com>
2022-03-02 23:39:29 +0300

    Create comparison.py
    comparison examples

---
+++ comparison.py
@@ -0,0 +1,19 @@
+
+a,b,c,d = 10,10,20,5
+
+username = "hasanturkmen"
+password = "123456"
+
+result = (a == b)
+result = (a != b)
+result = (a == c)
+result = (a != c)
+result = (username == "hasanturkmen")
+result = (a > c)
+result = (a >= b)
+result = (a <= b)
+result = (True == 1)
+result = (False == 0)
+result = False + True
+
+print(result)
