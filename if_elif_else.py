58b0371d1b302f0ae5773b9bef521e9c805185fc
Hasan Türkmen <wd.devcodec@gmail.com>
2022-03-03 21:42:33 +0300

    if_elif_else
    if_elif_else examples
    
    (elif kalp polat)

---
+++ if_elif_else.py
@@ -0,0 +1,24 @@
+
+a = 10
+b = 10
+
+if (a > b):
+    print("a is greater than b")
+elif(a == b):
+    print("a is equal to b")
+else:
+    print("b is greater than a")
+
+
+note = 85
+
+if (note < 50):
+    print("grade retention.")
+elif (note >= 50 and note < 70):
+    print("successful")
+elif (note >= 70 and note < 85):
+    print("honor certificate")
+else:
+    print("certificate of appreciation")
+
+
