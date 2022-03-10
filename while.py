655792e445223290fbd325ded99b1f6f7ce5bac4
Hasan Türkmen <wd.devcodec@gmail.com>
2022-03-11 02:30:40 +0300

    while sample

---
+++ while.py
@@ -0,0 +1,22 @@
+
+"""i=0
+while(i<10):
+    print(i)
+    i+=1"""
+"""
+i = 1
+while (i<=100):
+    if (i%2 == 0):
+        print(f"even number: {i}")
+    else:
+        print(f"odd number: {i}")
+
+    i += 1
+
+"""
+
+email = ""
+while not email:
+    email = input("email adresinizi giriniz: ");
+
+print(email)
