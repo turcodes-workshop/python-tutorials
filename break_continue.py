8a3a9c5fed4407055e812f476bdceb2335b6673c
Hasan Türkmen <wd.devcodec@gmail.com>
2022-03-11 02:52:20 +0300

    update break_continue.py
    updated.

---
+++ break_continue.py
@@ -0,0 +1,25 @@
+name = "Hasan"
+"""
+for c in name:
+    if(c == "a"):
+        break;
+    print(c)
+"""
+"""
+i = 0
+while (i<26):
+    i += 1
+    if(i%5 == 0):
+        continue
+    print(i)
+
+print("loop ended.")
+"""
+i,sum = 0,0
+while (i<= 100):
+    i+=1
+    if(i%2 == 1):
+        continue;
+    sum += i
+
+print(sum)
