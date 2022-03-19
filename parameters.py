0f866ba76bc69fa61b94efe8b492918cdfd3b754
Hasan Türkmen <wd.devcodec@gmail.com>
2022-03-19 16:36:57 +0300

    parameters-returns sample

---
+++ parameters.py
@@ -0,0 +1,19 @@
+
+
+from datetime import date, datetime
+
+
+def sum(num1:int, num2:int):
+    return num1+num2
+
+
+result = sum(123,10)
+
+def ageCalc(birthYear:int):
+    return 2022 - birthYear
+
+
+result = ageCalc(1995)
+
+print(result)
+
---
+++ returns.py
@@ -0,0 +1,6 @@
+def whatIsName():
+    name = input("What is your name: ")
+    return name
+
+name = whatIsName()
+print(f"your name: {name}")
